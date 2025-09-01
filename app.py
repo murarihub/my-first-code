27b94a8c0b1c0f7eb63dad4d83b3c079fd7593b9
murarihub <dhananjaymurari@gmail.com>
2025-09-01 09:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello from Render FastAPI!"}
+
+@app.get("/hello/{name}")
+def greet(name: str):
+    return {"message": f"Hello, {name}! Welcome to my API."}
